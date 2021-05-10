738b29399f01d9df032051147be98939595d21c4
Martini024 <martini.zhao@gmail.com>
2021-05-10 13:59:17 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import csvFile from './wpforms-1222-Upload-Form-2021-05-10-11-58-46.csv'
+
+import './App.css'
+
+import logo from './logo.svg'
+import getURLsFromCSV from './utils/getURLsFromCSV'
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const handleClick = () => {
+		getURLsFromCSV(csvFile)
+	}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Batch download files from urls inside csv file.</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<Button type="primary" onClick={handleClick}>
+					Generate Zip
+				</Button>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/utils/getFileExtension.js
@@ -0,0 +1,3 @@
+export default function getFileExtension(url) {
+	return url.split('.').pop()
+}
